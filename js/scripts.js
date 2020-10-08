00f3e6c42b319eb6866e914f51ed22d86a1438db
Anjali B N <bnanjali@gmail.com>
2020-10-08 14:55:00 +0530

    Adding version 1.0 of Global Link Traders website

---
+++ js/scripts.js
@@ -0,0 +1,353 @@
+const sr = ScrollReveal();
+
+// index.html
+
+sr.reveal('.mainHeading', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.headingCaption', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.aboutHeading', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.aboutPara', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.productsHeading', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.productsPara', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.productsCard', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.productsTitle', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.productsDescription', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.productsBtn', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.clientsHeading', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.clientsPara', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.clientsContainer', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.clientImage', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.clientImageCaption', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.clientDept', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.contactHeading', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.contactCaption', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.faIcons', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.contactHeaders', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.contactDetails', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.address', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.googleMap', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+// Product categories
+
+sr.reveal('.categoryHeading', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.categoryCaption', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.categoryCard', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.categoryImg', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.categoryBtn', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
+
+sr.reveal('.categoryPoint', {
+    origin: 'bottom',
+    distance: '15px',
+    duration: 500,
+    reset: true,
+    mobile: true,
+    desktop: true,
+    useDelay: 'always',
+    easing: 'ease-in-out',
+    viewFactor: 0.0
+});
